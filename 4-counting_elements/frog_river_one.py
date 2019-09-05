f2a238e415b612fb742924485a034572346c8d6e
luiyivp <31863570+luiyivp@users.noreply.github.com>
2019-09-05 11:58:31 -0400

    Update frog_river_one.py

--- 4-counting_elements/frog_river_one.py
+++ 4-counting_elements/frog_river_one.py
@@ -1,21 +1,19 @@
 def solution(X, A):
     i = 0
     sec = 0
-    counter = [0] * X
-    B = [0] * X    
-    n = len(B)
-    b_sum = n * (n + 1) // 2
+    counter = [0] * X  
+    n = len(counter)
+    total = n * (n + 1) // 2
     aux = 0
 
     for i in range(n):
-      B[i] = i + 1
+      counter[i] = i + 1
 
     for num in A:
-      counter[num - 1] += 1
-      aux += B[num - 1]
-      B[num - 1] = 0
+      aux += counter[num - 1]
+      counter[num - 1] = 0
 
-      if aux == b_sum:
+      if aux == total:
         return sec
       sec += 1      
     
